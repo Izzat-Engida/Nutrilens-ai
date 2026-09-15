f39eced6d5ba66269952f59e8d11ce333f444c1a
Izzat Engida <izzatengida7@gmail.com>
2026-09-15 15:43:40 +0300

    add: redux setup things

--- app/src/store/api/apiSlice.ts
+++ app/src/store/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { RootState } from '../index';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://192.168.1.11:8000',
+  baseUrl: `${process.env.API}:8000`,
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.accessToken;
 
